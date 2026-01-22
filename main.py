b27fa733975ea206be21f101a8aa93546d58ea30
Lennart van der Peet <96424381+LDvanderPeet@users.noreply.github.com>
2026-01-22 17:20:29 +0100

    22/01: structured repository. Introduced .yaml file

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import yaml
+import torch
+import os
 
 
+def main(config_path="config.yaml"):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+
+    # train_loader, val_loader = get_dataloaders(config)
+    #
+    # model = build_sar_model(config)
+
+
+
+if __name__ == "__main__":
+    print("Hello, World!")
--- src/models.py
+++ src/models.py
@@ -1,11 +1,27 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from diffusers import UNet2DConditionModel
 
+
+class VAE(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(784, 400),
+            nn.ReLU(),
+            nn.Linear(400, 20)  # 10 for mean and 10 for log-variance
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(10, 400),
+            nn.ReLU(),
+            nn.Linear(400, 784),
+            nn.Sigmoid()
+        )
 
 class Generator(nn.Module):
     def __init__(self, latent_dim, img_shape):
-        super(Generator, self).__innit__()
+        super().__init__()
 
         def block(feat_in, feat_out, normalize=True):
             layers = [nn.Linear(feat_in, feat_out)]
